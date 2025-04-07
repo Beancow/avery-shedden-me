7329fe127445f650fa784bdcc0e36699af48dc5b
Beancow <avery.shedden@gmail.com>
2025-04-07 13:09:00 +0100

    Stripped back the default app and imported radix components

--- src/router/index.js
+++ src/router/index.js
@@ -8,14 +8,12 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      meta: {
+        title: 'Home',
+        requiresAuth: false,
+        description: 'Welcome to the home page',
+        keywords: 'home, welcome, vue',
+      },
     },
   ],
 })
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import WelcomeScreen from '../components/WelcomeMessage.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <WelcomeScreen />
   </main>
 </template>

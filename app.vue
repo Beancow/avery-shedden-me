738eb8463ef4a5cf5511bb409503cb8eb7c4a5a5
Beancow <avery.shedden@gmail.com>
2025-04-13 15:01:15 +0100

    Testing body bg colour change

--- app.vue
+++ app.vue
@@ -9,3 +9,15 @@ const isOpen = ref(false);
     <AnimatedBurgerIcon :is-open="isOpen" @click="isOpen = !isOpen" />
   </div>
 </template>
+
+<style scoped>
+html,
+body {
+  height: 100%;
+  margin: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #52014d;
+}
+</style>

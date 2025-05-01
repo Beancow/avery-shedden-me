a7035145675bcc710bbbd69764f195dbc8f3595d
Beancow <avery.shedden@gmail.com>
2025-05-01 13:33:48 +0100

    Trying to fix image loading on GH pages

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,4 @@
 import type { NextConfig } from "next";
-import path from "path";
 import process from "process";
 
 const isStaticBuild = process.env.BUILD_TARGET === "static";
@@ -10,16 +9,8 @@ console.log(
 );
 
 const nextConfig: NextConfig = {
-  config: {
-    turbo: {
-      root: path.join(__dirname, ".."),
-    },
-    serverExternalPackages: ["@radix-ui/themes"],
-  },
-
   ...(isStaticBuild && { output: "export" }),
-
-  ...(isStaticBuild && { basePath: `/${repoName}` }), // Add this line
+  ...(isStaticBuild && { basePath: `/${repoName}` }),
 
   images: {
     ...(isStaticBuild && { unoptimized: true }),

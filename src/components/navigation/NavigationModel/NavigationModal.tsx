5320107d871746c82bc9f3243771df021d38d327
Beancow <avery.shedden@gmail.com>
2025-05-09 07:40:11 +0100

    pushing to see on pages

--- src/components/navigation/NavigationModel/NavigationModal.tsx
+++ src/components/navigation/NavigationModel/NavigationModal.tsx
@@ -3,44 +3,52 @@ import { Cross2Icon, HamburgerMenuIcon } from "@radix-ui/react-icons";
 import { Button, Flex } from "@radix-ui/themes";
 import Link from "next/link";
 import { NavigationProps } from "../navigationProps";
+import styles from "./styles.module.css";
 
 export default function NavigationModal({
   navLinks,
   checkActive,
 }: NavigationProps) {
   return (
-    <div>
+    <div className={styles.navigationModalRoot}>
       <Dialog.Root>
         <Dialog.Trigger asChild>
           <Button variant="soft" size="3">
             <HamburgerMenuIcon width="24" height="24" />
           </Button>
         </Dialog.Trigger>
-
         <Dialog.Portal>
-          <Dialog.Overlay />
+          <Dialog.Overlay className={styles.dialogOverlay} />
+          <Dialog.Content className={styles.dialogContent}>
+            <Dialog.Title className={styles.dialogTitle}>
+              Navigation Menu
+            </Dialog.Title>
 
-          <Dialog.Content title="Navigation Menu">
-            <Dialog.Title>Navigation Menu</Dialog.Title>
             <Dialog.Close asChild>
-              <Button variant="ghost" size="3" aria-label="Close">
+              <Button
+                variant="ghost"
+                size="3"
+                aria-label="Close"
+                className={styles.dialogCloseButton}
+              >
                 <Cross2Icon width="24" height="24" />
               </Button>
             </Dialog.Close>
 
             <Flex
               direction="column"
-              gap="4"
+              gap="4" // Radix Flex gap prop
               align="center"
               justify="center"
-              height="100%"
+              height="100%" // Ensure Flex takes full height of Dialog.Content
             >
               {navLinks.map((link) => (
+                // Dialog.Close ensures clicking a link closes the modal
                 <Dialog.Close asChild key={link.href}>
                   <Link
                     href={link.href}
-                    className={`.glowItem ${
-                      checkActive(link.href) ? ".active" : ""
+                    className={`${styles.mobileNavLink} glowItem ${
+                      checkActive(link.href) ? "active" : ""
                     }`}
                   >
                     {link.label}

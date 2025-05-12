17bb9f94b431192135c3d5a4a4ccba893691199c
Beancow <avery.shedden@gmail.com>
2025-05-12 19:09:25 +0100

    param name fix

--- src/app/projects/[[...tabs]]/page.tsx
+++ src/app/projects/[[...tabs]]/page.tsx
@@ -1,10 +1,10 @@
 import { ProjectTabs } from "../components/ProjectsTabs";
 
-export async function generateStaticParams() {
-  return [{ projects: "Tab1" }, { projects: "Tab2" }, { projects: "Tab3" }];
+export function generateStaticParams() {
+  return [{ projectId: "Tab1" }, { projectId: "Tab2" }, { projectId: "Tab3" }];
 }
 
-export default async function Page({
+export async function Page({
   params,
 }: {
   params: Promise<{ projectId: string }>;

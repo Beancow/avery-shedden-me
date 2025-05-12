42cbd8e56af69c1d8c3dd38600e02110f31628e9
Avery <avery.shedden@gmail.com>
2025-05-12 20:12:31 +0100

    Update page.tsx
    static page path came out wrong so hopefully this fixes the issue

--- src/app/projects/[[...tabs]]/page.tsx
+++ src/app/projects/[[...tabs]]/page.tsx
@@ -1,7 +1,9 @@
 import { ProjectTabs } from "../components/ProjectsTabs";
 
 export const generateStaticParams = async () => [
-  { tabs: ["Tab1", "Tab2", "Tab3"] },
+  { tabs: ["Tab1"] },
+  { tabs: ["Tab2"] },
+  { tabs: ["Tab3"]},
 ];
 
 export default async function Page({

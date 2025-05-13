3b15c7525e8dd78d34b3011853e5e74a0bf46af7
Beancow <avery.shedden@gmail.com>
2025-05-13 10:04:15 +0100

    fix: update env name

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import process from "process";
 
 const isStaticBuild = process.env.BUILD_TARGET === "static";
-const repoName = process.env.REPO_NAME;
+const repoName = process.env.NEXT_PUBLIC_REPO_NAME;
 
 console.log(
   `Building for: ${isStaticBuild ? "Static Export" : "SSR/Standard"}`

512309188ab1cc84c500c32ad04befcba494e0e2
Beancow <avery.shedden@gmail.com>
2025-05-20 09:29:47 +0100

    simplified panel toggle dynamic component
    - It was late and I was using a mix of localSotrage and react state. I was originally just trying to use cookie/local storage, but react state was just the simpler option when I am just adding this for testing

--- src/components/themePanelToggle/ThemePanelToggle.tsx
+++ src/components/themePanelToggle/ThemePanelToggle.tsx
@@ -3,21 +3,10 @@ import { Button, ThemePanel, useThemeContext } from "@radix-ui/themes";
 import { useState } from "react";
 
 export default function ThemePanelToggle() {
-  const panelOpen = useState(() => {
-    const storedValue = localStorage.getItem("themePanelOpen");
-    return storedValue === "true" ? true : false;
-  });
-  const [isPanelOpen, setIsPanelOpen] = panelOpen;
-
-  const themeContext = useThemeContext();
-
-  themeContext;
+  const [isPanelOpen, setPanelOpenState] = useState(false);
 
   const togglePanel = () => {
-    const currentState = localStorage.getItem("themePanelOpen") === "true";
-    localStorage.setItem("themePanelOpen", (!currentState).toString());
-    window.dispatchEvent(new Event("storage"));
-    setIsPanelOpen(!currentState);
+    setPanelOpenState((prev) => !prev);
   };
 
   return (

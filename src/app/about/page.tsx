0d812a5b569d9dc6eaf8a56aae567bb6cb67bf19
Beancow <avery.shedden@gmail.com>
2025-05-27 12:54:03 +0100

    feat: Populated missing pages

---
+++ src/app/about/page.tsx
@@ -0,0 +1,14 @@
+export default function AboutIndexPage() {
+  return (
+    <div>
+      <h1>About/Menu</h1>
+      <p>
+        Welcome to the about page! Here you can find information about our
+        project.
+      </p>
+      <p>
+        Explore our interests and values, and learn more about what drives us.
+      </p>
+    </div>
+  );
+}
